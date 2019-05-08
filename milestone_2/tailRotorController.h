0d50e93718b258f7c32b088c9428adaac432c783
Harry Dobbs <hdo27@uclive.ac.nz>
2019-05-09 08:35:30 +1200

    upsd

--- milestone_2/tailRotorController.h
+++ milestone_2/tailRotorController.h
@@ -17,5 +17,8 @@ void setTailPWM (uint32_t ui32Freq, uint32_t ui32Duty);
 
 void initialiseTailRotorPWM (void);
 
+#define PWM_TAIL_BASE        PWM1_BASE
+#define PWM_TAIL_OUTBIT      PWM_OUT_5_BIT
+
 
 #endif /* MAIN_ROTOR_CONTROLLER_H_ */
