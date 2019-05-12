49ccdcb3c307b19441078f33fa0ca2dcc754b790
Harry Dobbs <hdo27@uclive.ac.nz>
2019-05-12 22:27:43 +1200

    latest version

--- milestone_2/Debug/uartCommunication.d
+++ milestone_2/Debug/uartCommunication.d
@@ -1,47 +1,47 @@
 # FIXED
 
 uartCommunication.obj: ../uartCommunication.c
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
-uartCommunication.obj: /home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdio.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
-uartCommunication.obj: /home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+uartCommunication.obj: /home/harry/ti/TivaWare/inc/hw_memmap.h
+uartCommunication.obj: /home/harry/ti/TivaWare/inc/hw_types.h
+uartCommunication.obj: /home/harry/ti/TivaWare/inc/hw_ints.h
+uartCommunication.obj: /home/harry/ti/TivaWare/driverlib/gpio.h
+uartCommunication.obj: /home/harry/ti/TivaWare/driverlib/uart.h
+uartCommunication.obj: /home/harry/ti/TivaWare/driverlib/sysctl.h
+uartCommunication.obj: /home/harry/ti/TivaWare/driverlib/systick.h
+uartCommunication.obj: /home/harry/ti/TivaWare/driverlib/debug.h
+uartCommunication.obj: /home/harry/ti/TivaWare/driverlib/pin_map.h
+uartCommunication.obj: /home/harry/ti/TivaWare/utils/ustdlib.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdio.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+uartCommunication.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h
 uartCommunication.obj: ../OrbitOLED/OrbitOLEDInterface.h
 uartCommunication.obj: ../buttons4.h
 
 ../uartCommunication.c: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
-/home/harrydobbs/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdio.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
-/home/harrydobbs/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+/home/harry/ti/TivaWare/inc/hw_memmap.h: 
+/home/harry/ti/TivaWare/inc/hw_types.h: 
+/home/harry/ti/TivaWare/inc/hw_ints.h: 
+/home/harry/ti/TivaWare/driverlib/gpio.h: 
+/home/harry/ti/TivaWare/driverlib/uart.h: 
+/home/harry/ti/TivaWare/driverlib/sysctl.h: 
+/home/harry/ti/TivaWare/driverlib/systick.h: 
+/home/harry/ti/TivaWare/driverlib/debug.h: 
+/home/harry/ti/TivaWare/driverlib/pin_map.h: 
+/home/harry/ti/TivaWare/utils/ustdlib.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdio.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h: 
 ../OrbitOLED/OrbitOLEDInterface.h: 
 ../buttons4.h: 
