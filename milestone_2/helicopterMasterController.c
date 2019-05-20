28a6535bebee63822de9e652401e5767567e8628
spu30 <spu30@uclive.ac.nz>
2019-05-21 00:02:12 +1200

    changed UART to display mode

--- milestone_2/helicopterMasterController.c
+++ milestone_2/helicopterMasterController.c
@@ -158,7 +158,7 @@ int main(void)
     initADC();
     initDisplay();
     initButtons();
-    initReset();
+    //initReset();
     yawFSMInit();
     initialiseUSB_UART();
 
@@ -196,7 +196,7 @@ int main(void)
             currentDisplayAngle = findDisplayAngle(currentAngle);
             displayheight = heightAsPercentage(maxHeight, currentHeight, groundReference);
             char string[128];
-            usprintf(string, "------------\n\rYaw: %5d [%5d]\n\rTail: %5d\n\rHeight: %5d [%5d]\n\rMain: %5d\n\r", currentDisplayAngle, dersiredDisplayAngle, tailDutyCycle, displayheight, desiredHeightPercentage, mainDutyCycle);
+            usprintf(string, "Yaw: %5d [%5d]\n\rTail: %5d\n\rHeight: %5d [%5d]\n\rMain: %5d\n\rMode: %5d\n\r", currentDisplayAngle, dersiredDisplayAngle, tailDutyCycle, displayheight, desiredHeightPercentage, mainDutyCycle, flightMode);
             UARTSend(string);
         }
 
