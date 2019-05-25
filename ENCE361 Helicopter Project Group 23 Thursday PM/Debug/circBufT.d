4e18d62ebb73eca6a5d80111e704b7893a0e7dc0
Harry Dobbs <hdo27@uclive.ac.nz>
2019-05-25 15:01:41 +1200

    final heli

---
+++ ENCE361 Helicopter Project Group 23 Thursday PM/Debug/circBufT.d
@@ -0,0 +1,13 @@
+# FIXED
+
+circBufT.obj: ../circBufT.c
+circBufT.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+circBufT.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h
+circBufT.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+circBufT.obj: ../circBufT.h
+
+../circBufT.c: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdlib.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+../circBufT.h: 
