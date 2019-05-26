375d3be2aebe90e2106d61406ec217312c9a8207
Harry Dobbs <hdo27@uclive.ac.nz>
2019-05-26 16:03:59 +1200

    updated out file

---
+++ Helicopter_Final/Debug/helicopterMain.d
@@ -0,0 +1,59 @@
+# FIXED
+
+helicopterMain.obj: ../helicopterMain.c
+helicopterMain.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+helicopterMain.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+helicopterMain.obj: /home/harry/ti/TivaWare/inc/hw_memmap.h
+helicopterMain.obj: /home/harry/ti/TivaWare/inc/hw_types.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/adc.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/pwm.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/gpio.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/sysctl.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/systick.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/interrupt.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/debug.h
+helicopterMain.obj: /home/harry/ti/TivaWare/utils/ustdlib.h
+helicopterMain.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+helicopterMain.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h
+helicopterMain.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+helicopterMain.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h
+helicopterMain.obj: ../circBufT.h
+helicopterMain.obj: ../OrbitOLED/OrbitOLEDInterface.h
+helicopterMain.obj: ../buttons4.h
+helicopterMain.obj: ../helicopterAltitude.h
+helicopterMain.obj: ../userDisplay.h
+helicopterMain.obj: ../yawFiniteStateMachine.h
+helicopterMain.obj: ../mainRotorController.h
+helicopterMain.obj: ../tailRotorController.h
+helicopterMain.obj: ../uartCommunication.h
+helicopterMain.obj: /home/harry/ti/TivaWare/driverlib/uart.h
+helicopterMain.obj: ../helperFunctions.h
+
+../helicopterMain.c: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+/home/harry/ti/TivaWare/inc/hw_memmap.h: 
+/home/harry/ti/TivaWare/inc/hw_types.h: 
+/home/harry/ti/TivaWare/driverlib/adc.h: 
+/home/harry/ti/TivaWare/driverlib/pwm.h: 
+/home/harry/ti/TivaWare/driverlib/gpio.h: 
+/home/harry/ti/TivaWare/driverlib/sysctl.h: 
+/home/harry/ti/TivaWare/driverlib/systick.h: 
+/home/harry/ti/TivaWare/driverlib/interrupt.h: 
+/home/harry/ti/TivaWare/driverlib/debug.h: 
+/home/harry/ti/TivaWare/utils/ustdlib.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h: 
+../circBufT.h: 
+../OrbitOLED/OrbitOLEDInterface.h: 
+../buttons4.h: 
+../helicopterAltitude.h: 
+../userDisplay.h: 
+../yawFiniteStateMachine.h: 
+../mainRotorController.h: 
+../tailRotorController.h: 
+../uartCommunication.h: 
+/home/harry/ti/TivaWare/driverlib/uart.h: 
+../helperFunctions.h: 
---
+++ Helicopter_Final/Debug/yawFiniteStateMachine.d
@@ -0,0 +1,41 @@
+# FIXED
+
+yawFiniteStateMachine.obj: ../yawFiniteStateMachine.c
+yawFiniteStateMachine.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+yawFiniteStateMachine.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/inc/hw_memmap.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/inc/hw_types.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/driverlib/adc.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/driverlib/pwm.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/driverlib/gpio.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/driverlib/sysctl.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/driverlib/systick.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/driverlib/interrupt.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/driverlib/debug.h
+yawFiniteStateMachine.obj: /home/harry/ti/TivaWare/utils/ustdlib.h
+yawFiniteStateMachine.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+yawFiniteStateMachine.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h
+yawFiniteStateMachine.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+yawFiniteStateMachine.obj: /home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h
+yawFiniteStateMachine.obj: ../circBufT.h
+yawFiniteStateMachine.obj: ../OrbitOLED/OrbitOLEDInterface.h
+
+../yawFiniteStateMachine.c: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+/home/harry/ti/TivaWare/inc/hw_memmap.h: 
+/home/harry/ti/TivaWare/inc/hw_types.h: 
+/home/harry/ti/TivaWare/driverlib/adc.h: 
+/home/harry/ti/TivaWare/driverlib/pwm.h: 
+/home/harry/ti/TivaWare/driverlib/gpio.h: 
+/home/harry/ti/TivaWare/driverlib/sysctl.h: 
+/home/harry/ti/TivaWare/driverlib/systick.h: 
+/home/harry/ti/TivaWare/driverlib/interrupt.h: 
+/home/harry/ti/TivaWare/driverlib/debug.h: 
+/home/harry/ti/TivaWare/utils/ustdlib.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+/home/harry/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h: 
+../circBufT.h: 
+../OrbitOLED/OrbitOLEDInterface.h: 
