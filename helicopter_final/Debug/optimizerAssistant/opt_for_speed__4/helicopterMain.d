71f510364a4dbdfa6341779b852038983b0e769b
Harry Dobbs <hdo27@canterbury.ac.nz>
2019-06-02 22:46:05 +1200

    added milestone_1

---
+++ helicopter_final/Debug/optimizerAssistant/opt_for_speed__4/helicopterMain.d
@@ -0,0 +1,61 @@
+# FIXED
+
+helicopterMain.obj: ../helicopterMain.c
+helicopterMain.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h
+helicopterMain.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pwm.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h
+helicopterMain.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h
+helicopterMain.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h
+helicopterMain.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h
+helicopterMain.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h
+helicopterMain.obj: ../circBufT.h
+helicopterMain.obj: ../OrbitOLED/OrbitOLEDInterface.h
+helicopterMain.obj: ../buttons4.h
+helicopterMain.obj: ../helicopterAltitude.h
+helicopterMain.obj: ../userDisplay.h
+helicopterMain.obj: ../yawFiniteStateMachine.h
+helicopterMain.obj: ../mainRotorController.h
+helicopterMain.obj: ../tailRotorController.h
+helicopterMain.obj: ../uartCommunication.h
+helicopterMain.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+helicopterMain.obj: ../helperFunctions.h
+helicopterMain.obj: ../helicopterMain.h
+
+../helicopterMain.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pwm.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/ustdlib.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/stdarg.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/time.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/linkage.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.6.LTS/include/abi_prefix.h: 
+../circBufT.h: 
+../OrbitOLED/OrbitOLEDInterface.h: 
+../buttons4.h: 
+../helicopterAltitude.h: 
+../userDisplay.h: 
+../yawFiniteStateMachine.h: 
+../mainRotorController.h: 
+../tailRotorController.h: 
+../uartCommunication.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+../helperFunctions.h: 
+../helicopterMain.h: 
